48b306b080dc3fb7e143cdca14751b6aa1b3d1aa
Elizabeth Rios-Mujica <elizabeth.rmujica@gmail.com>
2022-06-28 12:29:39 -0400

    commit tree 8278c84615a389edb6913f93af77bc5973de8ad7

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,9 +7,12 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    author = Author.create(author_params)
-
-    render json: author, status: :created
+    author = Author.create!(author_params)
+    # if bird.valid?
+      render json: author, status: :created
+    # else
+  rescue ActiveRecord::RecordInvalid => invalid
+      render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   private
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 100 }
 end
